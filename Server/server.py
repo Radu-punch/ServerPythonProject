c06865dfd4bb3048270bfb1f63bb42132555a680
Condurari Radu <brockbraun@gmail.com>
2021-10-31 17:40:18 +0200

    am facut parsing la requesturile de la client si am asignat header la fisiere de test , am creat fisierele de test(Browser)

--- Server/server.py
+++ Server/server.py
@@ -1,29 +1,75 @@
 import socket
+import threading
+import re
+http_status={
+    '200': 'HTTP/1.1 200 OK',
+    '400': 'HTTP/1.1 400 Bad Request',
+    '404': 'HTTP/1.1 404 Not Found'
+}
+file_types={
+    'html': 'Content-Type: text/html; charset=utf-8',
+    'jpg': 'Content-Type: image/jpeg',
+    'png': 'Content-Type: image/png',
+    'css': 'Content-Type: text/css',
+    'ico': 'Content-Type: image/x-icon'
+}
 
 def request_parser(request):
-    buff=request.split(' ')
-    req_met=buff[0]
-    url=buff[1]
-    return (req_met,url)
+    buff=request.decode().split('\r\n')
+    method, request, protocol = buff[0].split(' ')
+    if request == '/': request = '/index.html'
+    path = re.findall('[/a-z.]+', request)[0]
+    file_type = path.split('.')[1]
+    return method,path,file_type
+
+def give_header(method,file_type):
+    return http_status[method].encode() +'\r\n'\
+           + file_types[file_type].encode() + '\r\n\r\n'
+
 
 def server_response(request):
-    req_met , url = request_parser(request)
+    method, path, file_type = request_parser(request)
+    #if method=="GET":
+    try:
+        with open('views'+path,'rb') as file:
+            data=file.read()
+            header=give_header('200',file_type)
+            return  header+data
+    except FileNotFoundError:
+            header=give_header('404',file_type)
+            err_mes='''
+        <html>
+            <head><title>404 PAGE NOT FOUND</title></head>
+            <body><h1>404 PAGE NOT FOUND</h1></body>
+        </html>
+        '''
+            return header+err_mes
 
 
+
+def MentinanceReDirect(url):
+    response_header = 'HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8\r\n\r\n'+url
+    return response_header
+def StopServer(client):
+    client.close()
 def run():
-    socket_server=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    socket_server.bind(('localhost',16666))
-    socket_server.listen()
+    try:
+        socket_server=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        socket_server.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+        socket_server.bind(('localhost',16666))
+        socket_server.listen()
 
-    while True:
-        client , adresa = socket_server.accept()
-        request = client.recv(1024)
-        print(request.decode('utf-8'))
-        print()
-        print(adresa)
-        response = server_response(request.decode('utf-8'))
-        client.sendall('Merge Serverul'.encode())
-        client.close()
+        while True:
+            client , adresa = socket_server.accept()
+            request = client.recv(4096)
+            print(request.decode('utf-8'))
+            print()
+            print(adresa)
+            response = server_response(request.decode('utf-8'))
+            client.sendall(response)
+            client.shutdown(socket.SHUT_WR)
+    except KeyboardInterrupt:
+        socket_server.close()
 
 if __name__ == '__main__':
     run()
