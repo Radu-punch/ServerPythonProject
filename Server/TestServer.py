843aba829be34a7f07afa021537d2c5a21d935bd
Condurari Radu <brockbraun@gmail.com>
2021-11-06 15:30:12 +0200

    am facut varianta cu thread-uri .

--- Server/TestServer.py
+++ Server/TestServer.py
@@ -1,56 +1,61 @@
-from unittest import IsolatedAsyncioTestCase , main,mock
+from unittest import TestCase, main, mock
 from Server.server import *
 from Server.views import *
+import sys
 import socket
-class ServerTest(IsolatedAsyncioTestCase):
-    async def test_checkport_default(self):
-             res = await check_port(33)
-             self.assertEqual(res , 16666)
 
-    async def test_checkport_usual(self):
-            res =  await check_port(4444)
-            self.assertEqual(res,4444)
 
-    async def test_give_header(self):
-            ras='HTTP/1.1 200 OK'.encode()+'\r\n'.encode()+'Content-Type: text/html; charset=utf-8'.encode()+'\r\n\r\n'.encode()
-            res = await give_header('200','html')
-            self.assertEqual(res,ras)
+class ServerTest(TestCase):
+    def test_checkport_default(self):
+        res = check_port(33)
+        self.assertEqual(res, 16666)
 
-    async def test_give_header_code_404(self):
-            ras='HTTP/1.1 404 Not Found'.encode()+'\r\n'.encode()+'Content-Type: text/css'.encode()+'\r\n\r\n'.encode()
-            res=await give_header('404','css')
-            self.assertEqual(res,ras)
+    def test_checkport_usual(self):
+        res = check_port(4444)
+        self.assertEqual(res, 4444)
 
-    async def test_request_parser(self):
-            req='GET /favicon.ico HTTP/1.1'.encode()
-            res = await request_parser(req)
-            self.assertEqual(res,('GET','/favicon.ico','ico'))
-    async def test_server_responce_501(self):
-            req='HTTP/1.1 501 Not Implemented\r\nContent-Type: text/html; charset=utf-8'.encode()+'\r\n\r\n'.encode()+'''
+    def test_give_header(self):
+        ras = 'HTTP/1.1 200 OK'.encode() + '\r\n'.encode() + 'Content-Type: text/html; charset=utf-8'.encode() + '\r\n\r\n'.encode()
+        res = give_header('200', 'html')
+        self.assertEqual(res, ras)
+
+    def test_give_header_code_404(self):
+        ras = 'HTTP/1.1 404 Not Found'.encode() + '\r\n'.encode() + 'Content-Type: text/css'.encode() + '\r\n\r\n'.encode()
+        res = give_header('404', 'css')
+        self.assertEqual(res, ras)
+
+    def test_request_parser(self):
+        req = 'GET /favicon.ico HTTP/1.1'.encode()
+        res = request_parser(req)
+        self.assertEqual(res, ('GET', '/favicon.ico', 'ico'))
+
+    def test_server_responce_501(self):
+        req = 'HTTP/1.1 501 Not Implemented\r\nContent-Type: text/html; charset=utf-8'.encode() + '\r\n\r\n'.encode() + '''
                 <html>
                       <head><title>501 NOT IMPLEMENTED</title></head>
                       <body><h1>501 NOT IMPLEMENTED</h1></body>
                 </html>
                  '''.encode()
-            rasp= await server_response('OPTIONS /favicon.ico HTTP/1.1'.encode())
-            self.assertEqual(rasp,req)
-    async def test_server_responce_404(self):
-            ras='HTTP/1.1 404 Not Found'.encode()+'\r\n'.encode()+'Content-Type: text/html; charset=utf-8'.encode()\
-                +'\r\n\r\n'.encode()+'''
+        rasp = server_response('OPTIONS /favicon.ico HTTP/1.1'.encode())
+        self.assertEqual(rasp, req)
+
+    def test_server_responce_404(self):
+        ras = 'HTTP/1.1 404 Not Found'.encode() + '\r\n'.encode() + 'Content-Type: text/html; charset=utf-8'.encode() \
+              + '\r\n\r\n'.encode() + '''
                 <html>
                     <head><title>404 PAGE NOT FOUND</title></head>
                     <body><h1>404 PAGE NOT FOUND</h1></body>
                 </html>
                         '''.encode()
 
-            req =  await error_404()
-            self.assertEqual(req,ras)
+        req = error_404()
+        self.assertEqual(req, ras)
 
-    async def test_exception_server_responce(self):
+    def test_exception_server_responce(self):
         with mock.patch('server.server_response', side_effect=FileNotFoundError, create=True):
-            rasp = await server_response('GET /aaaa.html HTTP/1.1'.encode())
-            req = ras='HTTP/1.1 404 Not Found'.encode()+'\r\n'.encode()+'Content-Type: text/html; charset=utf-8'.encode()\
-                +'\r\n\r\n'.encode()+'''
+            rasp = server_response('GET /aaaa.html HTTP/1.1'.encode())
+            req = ras = 'HTTP/1.1 404 Not Found'.encode() + '\r\n'.encode() + 'Content-Type: text/html; charset=utf-8'.encode() \
+                        + '\r\n\r\n'.encode() + '''
                 <html>
                     <head><title>404 PAGE NOT FOUND</title></head>
                     <body><h1>404 PAGE NOT FOUND</h1></body>
@@ -58,17 +63,31 @@ class ServerTest(IsolatedAsyncioTestCase):
                         '''.encode()
         self.assertEqual(rasp, req)
 
-    async def test_mentenance(self):
-            ras='HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8\r\n\r\n'.encode()\
-              +'''<!doctype html>\r\n<html>\r\n<head>\r\n    <meta http-equiv="Content-type" content="text/html;charset=UTF-8">\r\n    <title>PythonWebServer</title>\r\n</head>\r\n<body>\r\n<h1>This site is under maintenance</h1>\r\n<p>Please come back later</p>\r\n</body>\r\n</html>'''.encode()
-            req=await MentinanceReDirect()
-            self.assertEqual(req,ras)
-    async def test_server_responce_200(self):
-        with mock.patch('server.server_response',  create=True):
-            rasp = await server_response('GET /LinkedFile.html HTTP/1.1'.encode())
-            req = 'HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8\r\n\r\n'.encode()+'''<!DOCTYPE html>\r\n<html lang="en">\r\n<head>\r\n    <meta charset="UTF-8">\r\n    <title>Abobus</title>\r\n</head>\r\n<body>\r\n<h1>second page</h1>\r\n        <p>caz de test pentru serverul meu</p>\r\n\r\n</body>\r\n</html>'''.encode()
-        self.assertEqual(rasp,req)
+    def test_mentenance(self):
+        ras = 'HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8\r\n\r\n'.encode() \
+              + '''<!doctype html>\r\n<html>\r\n<head>\r\n    <meta http-equiv="Content-type" content="text/html;charset=UTF-8">\r\n    <title>PythonWebServer</title>\r\n</head>\r\n<body>\r\n<h1>This site is under maintenance</h1>\r\n<p>Please come back later</p>\r\n</body>\r\n</html>'''.encode()
+        req = MentinanceReDirect()
+        self.assertEqual(req, ras)
 
+    def test_server_responce_200(self):
+        with mock.patch('server.server_response', create=True):
+            rasp = server_response('GET /LinkedFile.html HTTP/1.1'.encode())
+            req = 'HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8\r\n\r\n'.encode() + '''<!DOCTYPE html>\r\n<html lang="en">\r\n<head>\r\n    <meta charset="UTF-8">\r\n    <title>Abobus</title>\r\n</head>\r\n<body>\r\n<h1>second page</h1>\r\n        <p>caz de test pentru serverul meu</p>\r\n\r\n</body>\r\n</html>'''.encode()
+        self.assertEqual(rasp, req)
+
+    def test_conect_server(self):
+        sok = conect_to_socket(16000)
+        self.assertTrue(sok, True)
+
+    # def test_aleg_stare1(self):
+    #     socket_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    #     socket_server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    #     socket_server.bind(('localhost', 16000))
+    #     socket_server.listen()
+    #     client = socket_server.accept()
+    #     req = client.recv(4096)
+    #     # req='HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8\r\n\r\n'
+    #     self.assertTrue(aleg_stare(1, client, req), True)
 
 
 if __name__ == '__main__':
--- Server/server.py
+++ Server/server.py
@@ -2,63 +2,65 @@ import socket
 import threading
 import re
 import asyncio
-http_status={
+
+http_status = {
     '200': 'HTTP/1.1 200 OK',
     '400': 'HTTP/1.1 400 Bad Request',
     '404': 'HTTP/1.1 404 Not Found',
     '501': 'HTTP/1.1 501 Not Implemented'
 }
-file_types={
-    'html': 'Content-Type: text/html; charset=utf-8',
-    'jpg': 'Content-Type: image/jpeg',
-    'png': 'Content-Type: image/png',
-    'css': 'Content-Type: text/css',
-    'ico': 'Content-Type: image/x-icon'
-}
+file_types = {'html': 'Content-Type: text/html; charset=utf-8', 'jpg': 'Content-Type: image/jpeg',
+              'png': 'Content-Type: image/png', 'css': 'Content-Type: text/css', 'ico': 'Content-Type: image/x-icon'}
 
 
-async def request_parser(request):
-        buff=request.decode().split('\r\n')
-        method, request, protocol = buff[0].split(' ')
-        if request == '/': request = '/index.html'
-        path = re.findall('[/A-Za-z.]+', request)[0]
-        file_type = path.split('.')[1]
-        return method, path, file_type
+def request_parser(request):
+    buff = request.decode().split('\r\n')
+    method, request, protocol = buff[0].split()
+    if request == '/': request = '/index.html'
+    path = re.findall('[/A-Za-z0-9.]+', request)[0]
+    file_type = path.split('.')[1]
+    return method, path, file_type
 
-async def give_header(code,file_type):
-        return http_status[code].encode() +'\r\n'.encode()\
-               + file_types[file_type].encode() + '\r\n\r\n'.encode()
 
-async def check_port(port):
-        default_port=16666
-        if port>1024 and port<65000:
-            return port
-        else:
-            return default_port
+def give_header(code, file_type):
+    return http_status[code].encode() + '\r\n'.encode() \
+           + file_types[file_type].encode() + '\r\n\r\n'.encode()
 
-async def server_response(request):
-            method, path, file_type = await request_parser(request)
-            if method!="GET" and method!="HEAD":
-                    res = await error_501()
-                    return res
-            else:
-                try:
-                    with open('views'+path,'rb') as file:
-                        data=file.read()
-                        header= await give_header('200',file_type)
-                        return  header+data
-                except FileNotFoundError:
-                        res = await error_404()
-                        return res
 
-async def MentinanceReDirect():
-    f=open('views/mentenanta.html', 'rb')
-    data=f.read()
+def check_port(port):
+    default_port = 16666
+    if port > 1024 and port < 65000:
+        return port
+    else:
+        return default_port
+
+
+def server_response(request):
+    method, path, file_type = request_parser(request)
+    if method != "GET" and method != "HEAD":
+        res = error_501()
+        return res
+    else:
+        try:
+            with open('views' + path, 'rb') as file:
+                data = file.read()
+                header = give_header('200', file_type)
+                return header + data
+        except FileNotFoundError:
+            res = error_404()
+            return res
+
+
+def MentinanceReDirect():
+    f = open('views/mentenanta.html', 'rb')
+    data = f.read()
     response_header = 'HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8\r\n\r\n'.encode()
     f.close()
-    return response_header+data
-async def error_404():
-    header = await give_header('404', 'html')
+    return response_header + data
+
+
+def error_404():
+    header = give_header('404', 'html')
     err_mes = '''
                 <html>
                     <head><title>404 PAGE NOT FOUND</title></head>
@@ -66,8 +68,10 @@ async def error_404():
                 </html>
                         '''.encode()
     return header + err_mes
-async def error_501():
-    head = await give_header('501', 'html')
+
+
+def error_501():
+    head = give_header('501', 'html')
     err_m = '''
                 <html>
                       <head><title>501 NOT IMPLEMENTED</title></head>
@@ -76,32 +80,48 @@ async def error_501():
                  '''.encode()
     return head + err_m
 
-async def StopServer(client):
-    client.close()
+
+def conect_to_socket(port):
+    socket_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    socket_server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    socket_server.bind(('localhost', port))
+    socket_server.listen()
+    return socket_server
 
 
-async def run(port,stare):
+def aleg_stare(stare, client, request):
+    match stare:
+        case 1:
+            response = server_response(request)
+            client.sendall(response)
+        case 3:
+            ras = MentinanceReDirect()
+            client.sendall(ras)
+            client.shutdown(socket.SHUT_WR)
+        case 2:
+            client.close()
 
-        socket_server= socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        socket_server.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
-        port1= await check_port(port)
-        socket_server.bind(('localhost',port1))
-        socket_server.listen()
-        while True:
-            client , adresa = socket_server.accept()
-            request = client.recv(4096)
-            print(request.decode('utf-8'))
-            print()
-            print(adresa)
-            if stare==1:
-                response = await server_response(request)
-                client.sendall(response)
-            elif stare == 3:
-                ras = await MentinanceReDirect()
-                client.sendall(ras)
-                client.shutdown(socket.SHUT_WR)
+def get_stare():
+    stare = int(input("Da stare intre 1-3: "))
+    return stare
 
+def get_port():
+    port = int(input("da un port intre 1024-65000"))
+    return port
+
+def run():
+    stare = get_stare()
+    port = get_port()
+    port = check_port(port)
+    socket_server = conect_to_socket(port)
+    while True:
+        client, adresa = socket_server.accept()
+        request = client.recv(4096)
+        print(request.decode('utf-8'))
+        print()
+        print(adresa)
+        threading.Thread(target=aleg_stare, args=(stare, client, request)).run()
 
 
 if __name__ == '__main__':
-    asyncio.run(run(4000,1))
+    run()
